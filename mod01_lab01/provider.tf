09e3696cbfdd6063ad3a829b03ee29f8c090ff55
Felipe Albuquerque <fel.albuquerque@outlook.com>
2025-02-28 17:22:43 -0300

    labs mod 001

---
+++ mod01_lab01/provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "xxxx-xxxxxxx-xxxxxxxx-xxxxx-xxxx"
+  client_id       = "xxxx-xxxxxxx-xxxxxxxx-xxxxx-xxxx"
+  client_secret   = "xxxx-xxxxxxx-xxxxxxxx-xxxxx-xxxx"
+  tenant_id       = "xxxx-xxxxxxx-xxxxxxxx-xxxxx-xxxx"
+}
